7056e68a23bcda27fcf00a20161eca155de276ec
OdgerelJ <odie.jamsranjav@slalom.com>
2023-12-29 16:51:24 -0600

    notification added

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const timer = {
-  pomodoro: 25,
-  shortBreak: 5,
-  longBreak: 15,
+  pomodoro: 1,
+  shortBreak: 1,
+  longBreak: 1,
   longBreakInterval: 4,
+  sessions: 0,
 };
 
 let interval;
 
+const buttonSound = new Audio("button-sound.mp3");
 const mainButton = document.getElementById("js-btn");
 mainButton.addEventListener("click", () => {
+  buttonSound.play();
   const { action } = mainButton.dataset;
   if (action === "start") {
     startTimer();
@@ -33,6 +36,9 @@ function switchMode(mode) {
     .forEach((e) => e.classList.remove("active"));
   document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
   document.body.style.backgroundColor = `var(--${mode})`;
+  document
+    .getElementById("js-progress")
+    .setAttribute("max", timer.remainingTime.total);
 
   updateClock();
 }
@@ -65,6 +71,8 @@ function startTimer() {
   let { total } = timer.remainingTime;
   const endTime = Date.parse(new Date()) + total * 1000;
 
+  if (timer.mode === "pomodoro") timer.sessions++;
+
   mainButton.dataset.action = "stop";
   mainButton.textContent = "stop";
   mainButton.classList.add("active");
@@ -76,6 +84,27 @@ function startTimer() {
     total = timer.remainingTime.total;
     if (total <= 0) {
       clearInterval(interval);
+
+      switch (timer.mode) {
+        case "pomodoro":
+          if (timer.sessions % timer.longBreakInterval === 0) {
+            switchMode("longBreak");
+          } else {
+            switchMode("shortBreak");
+          }
+          break;
+        default:
+          switchMode("pomodoro");
+      }
+
+      if (Notification.permission === 'granted') {
+        const text =
+          timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+        new Notification(text);
+      }
+
+      document.querySelector(`[data-sound="${timer.mode}"]`).play();
+      startTimer();
     }
   }, 1000);
 }
@@ -98,8 +127,32 @@ function updateClock() {
 
   min.textContent = minutes;
   sec.textContent = seconds;
+
+  const text =
+    timer.mode === "pomodoro" ? "Get back to work!" : "Take a break!";
+  document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById("js-progress");
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  switchMode("pomodoro");
+document.addEventListener('DOMContentLoaded', () => {
+  // Let's check if the browser supports notifications
+  if ('Notification' in window) {
+    // If notification permissions have neither been granted or denied
+    if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
+      // ask the user for permission
+      Notification.requestPermission().then(function(permission) {
+        // If permission is granted
+        if (permission === 'granted') {
+          // Create a new notification
+          new Notification(
+            'Awesome! You will be notified at the start of each session'
+          );
+        }
+      });
+    }
+  }
+
+  switchMode('pomodoro');
 });
